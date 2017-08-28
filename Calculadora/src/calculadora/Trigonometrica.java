a2842d413ba02670f9779b3446dc0ba1f06aaef2
Estudiantes <Estudiantes@Labing70215.udistrital.edu.co>
2017-08-28 06:04:15 -0500

    calculadora,operaciones trigonometricas

---
+++ Calculadora/src/calculadora/Trigonometrica.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Trigonometrica extends Calculadora {
+    
+    
+
+
+    void cambiar(){
+    operando1=(float)Math.toRadians(operando1);
+            
+            }    
+            
+    void seno(){
+         cambiar();
+        resultado=(float)Math.sin(operando1);
+      
+        
+    }
+    
+     void coseno(){
+          cambiar();
+        resultado=(float)Math.cos(operando1);
+        
+    }
+     void tangente(){
+         cambiar();
+         resultado=(float)Math.tan(operando1);
+         
+     }
+ 
+}
