673518d0aa1623684871ca226ec4ebcdccb95146
Estudiantes <Estudiantes@Labing70215.udistrital.edu.co>
2017-08-28 05:34:50 -0500

    calculadora, aoperaciones basicas

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+float operando1;
+float operando2;
+float resultado;
+
+ 
+  
+        
+    void sumar(){
+        resultado=operando1+operando2;
+        
+        
+    }
+    
+    void restar(){
+        resultado=operando1-operando2;
+        
+    }
+    void multiplicar(){
+    resultado=operando1*operando2;
+    }
+
+    void dividir(){
+        
+        resultado=operando1/operando2;
+       
+    
+
+}
+
+
